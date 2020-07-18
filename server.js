cda961f52a51b2efed6775458df6f9bc13e9597c
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2020-07-18 13:23:16 -0400

    rename server/ to api/

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const next = require('next')
-const apiRoutes = require('./server/routes')
+const apiRoutes = require('./api/routes')
 const { courseQueue } = require('./util/queue')
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
