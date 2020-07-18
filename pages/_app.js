dc63ad350bc46218b2b9a4acf534f7c85e4ffda7
2xOu7 <jonathanou1998@gmail.com>
2020-07-17 23:23:30 -0400

    added basic loading bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React, { Component } from 'react'
 import 'semantic-ui-css/semantic.min.css'
+import Router from "next/router";
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
 
-function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+//Binding events. 
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
+export default class App extends Component {
+
+  render() {
+    const { Component, pageProps } = this.props
+    return <Component {...pageProps} />
+  }
 }
-
-export default App
