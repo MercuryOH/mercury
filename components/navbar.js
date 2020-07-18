900ab58c63df7c47fdba87e696624babf08de509
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2020-07-18 13:59:12 -0400

    update navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,13 +1,25 @@
 import React from 'react'
-import { Menu } from 'semantic-ui-react'
+import styled from 'styled-components'
+import { Menu, Image, Label } from 'semantic-ui-react'
+
+const ProfileContainer = styled.div`
+  display: flex;
+  align-items: center;
+`
 
 function Navbar() {
   return (
-    <Menu size="large" borderless>
+    <Menu size="massive" borderless>
       <Menu.Item header>Mercury</Menu.Item>
-      <Menu.Item>Dashboard</Menu.Item>
       <Menu.Menu position="right">
-        <Menu.Item>Logout</Menu.Item>
+        <ProfileContainer>
+          <Label>Fall 2020</Label>
+          <Image
+            src="https://images.unsplash.com/photo-1495211895963-08d8812dcbf0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+            style={{ marginLeft: 10, marginRight: 10 }}
+            avatar
+          />
+        </ProfileContainer>
       </Menu.Menu>
     </Menu>
   )
