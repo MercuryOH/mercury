c95772fc36a289a69a3defb288f6a8324580bd17
Claude-lu <49216508+Claude-lu@users.noreply.github.com>
2020-07-18 15:27:39 -0600

    Added calendar.js

---
+++ pages/calendar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/layout'
+import { Header } from 'semantic-ui-react'
+
+function Calendar() {
+  return (
+    <Layout left={<p>CALENDAR</p>} right={<p>CALENDAR</p>}>
+      <Header>CALENDAR</Header>
+    </Layout>
+  )
+}
+
+export default Calendar
