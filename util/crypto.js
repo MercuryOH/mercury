3868dba70df985a4aa39178b452868db5219d4f5
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2020-07-20 23:21:00 -0400

    add helper function verifyJWT

--- util/crypto.js
+++ util/crypto.js
@@ -11,3 +11,18 @@ const jwt = require('jsonwebtoken')
 exports.createJWT = (payload) => {
   return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '24h' })
 }
+
+/**
+ * Verifies a web token.
+ * If successfull, returns token's payload; otherwise
+ * throws error
+ *
+ * @param {string} token json web token to verify
+ *
+ * @throws {Error} token not valid error
+ *
+ * @returns {object} token's payload
+ */
+exports.verifyJWT = (token) => {
+  return jwt.verify(token, process.env.JWT_SECRET)
+}
