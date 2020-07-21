6abca2e677292f6932ab1348095d83b5e2296c73
Anya Ji <aj592@cornell.edu>
2020-07-21 18:26:55 -0400

    add calendar

--- pages/calendar.js
+++ pages/calendar.js
@@ -37,17 +37,21 @@ const friendOptions = [
 function Calendar() {
   return (
     <Layout
-    left=
-    <div>
-      <LargeLabel
-        content = {<p>Classes</p>}>
-      </LargeLabel>
-      <DropDown>
-      </DropDown>
-    </div>
-    right={<p>CALENDAR</p>}>
-      <Header>Hello</Header>
-      <button>Hello</button>
+      left={
+        <div>
+          <LargeLabel content={<p>Classes</p>}></LargeLabel>
+          <DropDown></DropDown>
+        </div>
+      }
+    >
+      <iframe
+        src="https://calendar.google.com/calendar/b/1/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%230B8043&mode=WEEK&showTitle=1"
+        style={{ border: '0' }}
+        width={'100%'}
+        height={'100%'}
+        frameborder={0}
+        scrolling="no"
+      ></iframe>
     </Layout>
   )
 }
