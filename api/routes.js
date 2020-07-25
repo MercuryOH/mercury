9af181092cf1649b8f241251cd5b5506289d5314
2xOu7 <jonathanou1998@gmail.com>
2020-07-25 14:22:55 -0400

    added router

--- api/routes.js
+++ api/routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 const users = require('./controllers/users')
 const channels = require('./controllers/channels')
+const classes = require('./controllers/classes')
 
 router.use('/users', users)
 router.use('/channels', channels)
+router.use('/classes', classes)
 module.exports = router
