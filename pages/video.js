ce685483c40999b14d967e65750ba739afa03df6
2xOu7 <jonathanou1998@gmail.com>
2020-07-27 21:10:35 -0400

    video.js

--- pages/video.js
+++ pages/video.js
@@ -6,7 +6,7 @@ export default class MyApp extends Component {
     return (
       <OTSession
         apiKey="46858704"
-        sessionId="your-session-id"
+        sessionId="2_MX40Njg1ODcwNH5-MTU5NTgwODMzNDIzOH5RUk9uQjRqRzg4VEdWUEwwOVJEVTA3UnF-UH4"
         token="your-session-token"
       >
         <OTPublisher />
