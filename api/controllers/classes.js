d5e452798415bf722c489b1356948855e5461453
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2020-07-29 20:13:35 -0400

    clean up classes controller

--- api/controllers/classes.js
+++ api/controllers/classes.js
@@ -1,9 +1,5 @@
 const router = require('express').Router()
-const models = require('../../models')
 const middleware = require('../../util/middleware')
-/**
- * Get the classes that the current user is enrolled into
- */
 
 router.get('/', middleware.authRequired, async (req, res) => {
   const classes = req.user.Classes.map((c) => ({
