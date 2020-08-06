3aab534aca6556f34d566405e6c18a0c3647ab6a
2xOu7 <jonathanou1998@gmail.com>
2020-08-06 14:49:46 -0400

    formatting

--- components/util/EventEmitter.js
+++ components/util/EventEmitter.js
@@ -1,9 +1,11 @@
 export const EventEmitter = {
   events: {},
+
   publish: function (event, data) {
     if (!this.events[event]) return
     this.events[event].forEach((callback) => callback(data))
   },
+
   subscribe: function (event, callback) {
     if (!this.events[event]) this.events[event] = []
     this.events[event].push(callback)
