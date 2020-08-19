d8eb38b03060b849306ebdd2eca2a803b37524b4
2xOu7 <jonathanou1998@gmail.com>
2020-08-18 22:18:12 -0400

    Revert "fixed issue #95"
    This reverts commit ab4a6182ed6d61800100473964a38f7449bb362e.

--- pages/classes/[classId].js
+++ pages/classes/[classId].js
@@ -232,11 +232,7 @@ class ClassPage extends Component {
   handleSelectGroup = async (group) => {
     const { role } = this.state.currentClass
 
-    if (
-      group.type === 'office' ||
-      group.type === 'discussion' ||
-      role === 'Professor'
-    ) {
+    if (group.type === 'office' || role === 'Professor') {
       // you are popped off the waiting queue or you are a TA
       this.joinGroup(group)
       return
