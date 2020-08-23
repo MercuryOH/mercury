88a982abbb6a1bcda173d8f1d0e9f1e7b68d778e
Anya Ji <aj592@cornell.edu>
2020-08-23 15:22:50 -0400

    modifyClasses ui; refactored dropdown to class

--- components/dropDown.js
+++ components/dropDown.js
@@ -4,34 +4,42 @@ import { useRouter } from 'next/router'
 import { AuthRequired } from '../components/authProvider'
 import * as api from '../util/mercuryService'
 import Link from 'next/Link'
+import ModifyClassesModal from './modifyClassesModal'
 
-function DropDown() {
-  const [classes, setClasses] = useState([])
+const colors = [
+  'red',
+  'orange',
+  'yellow',
+  'olive',
+  'green',
+  'teal',
+  'blue',
+  'violet',
+  'purple',
+  'pink',
+  'brown',
+  'gray',
+  'black',
+]
 
-  useEffect(() => {
+class DropDown extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      classes: [],
+    }
+  }
+
+  componentDidMount() {
     api
       .getClasses()
-      .then((classes) => setClasses(classes))
+      .then((classes) => this.setState({classes}))
       .catch(console.error)
-  }, [])
+  }
 
-  const colors = [
-    'red',
-    'orange',
-    'yellow',
-    'olive',
-    'green',
-    'teal',
-    'blue',
-    'violet',
-    'purple',
-    'pink',
-    'brown',
-    'gray',
-    'black',
-  ]
+ modifyClasses = () => {}
 
-  function zip() {
+  zip() {
     var args = [].slice.call(arguments)
     var shortest =
       args.length == 0
@@ -46,79 +54,97 @@ function DropDown() {
       })
     })
   }
-
-  return (
-    <div>
-      <Accordion
-        fluid
-        exclusive={false}
-        defaultActiveIndex={[0, 1]}
-        style = {{fontSize: '1vw'}}
-        panels={[
-          {
-            key: 'student',
-            title: 'Student',
-            content: {
-              content: (
-                <div>
-                  <List relaxed>
-                    <>
-                    {zip(colors,classes).filter((zipped) => zipped[1].role === 'Student').map((zipped) => (
-                      <List.Item key={`class_${zipped[1].id}`}>
-                        <List.Content>
-                          <Link href={`/classes/${zipped[1].id}`}>
-                            <Button
-                              style={{
-                                fontSize: '1vw',
-                                width: '100%',
-                                minWidth: '41px',
-                              }}
-                              color={zipped[0]}
-                              content={zipped[1].name}
-                            />
-                          </Link>
-                        </List.Content>
-                      </List.Item>
-                    ))}
-                    </>
-                  </List>
-                </div>
-              ),
-            },
-          },
-          {
-            key: 'ta',
-            title: 'TA',
-            content: {
-              content: (
-                <div>
-                  <List relaxed>
-                  {zip(colors,classes).filter((zipped) => zipped[1].role === 'Professor').map((zipped) => (
-                    <List.Item key={`class_${zipped[1].id}`}>
-                      <List.Content>
-                        <Link href={`/classes/${zipped[1].id}`}>
-                          <Button
-                            style={{
-                              fontSize: '1vw',
-                              width: '100%',
-                              minWidth: '41px',
-                            }}
-                            color={zipped[0]}
-                            content={zipped[1].name}
-                          />
-                        </Link>
-                      </List.Content>
-                    </List.Item>
-                  ))}
-                  </List>
-                </div>
-              ),
-            },
-          },
-        ]}
-      />
-    </div>
-  )
+  render() {
+    return (
+      <>
+        <div>
+          <Accordion
+            fluid
+            exclusive={false}
+            defaultActiveIndex={[0, 1]}
+            style={{ fontSize: '1vw' }}
+            panels={[
+              {
+                key: 'student',
+                title: 'Student',
+                content: {
+                  content: (
+                    <div>
+                      <List relaxed>
+                        <>
+                          {this.zip(colors, this.state.classes)
+                            .filter((zipped) => zipped[1].role === 'Student')
+                            .map((zipped) => (
+                              <List.Item key={`class_${zipped[1].id}`}>
+                                <List.Content>
+                                  <Link href={`/classes/${zipped[1].id}`}>
+                                    <Button
+                                      style={{
+                                        fontSize: '1vw',
+                                        width: '100%',
+                                        minWidth: '41px',
+                                      }}
+                                      color={zipped[0]}
+                                      content={zipped[1].name}
+                                    />
+                                  </Link>
+                                </List.Content>
+                              </List.Item>
+                            ))}
+                        </>
+                      </List>
+                    </div>
+                  ),
+                },
+              },
+              {
+                key: 'ta',
+                title: 'TA',
+                content: {
+                  content: (
+                    <div>
+                      <List relaxed>
+                        {this.zip(colors, this.state.classes)
+                          .filter((zipped) => zipped[1].role === 'Professor')
+                          .map((zipped) => (
+                            <List.Item key={`class_${zipped[1].id}`}>
+                              <List.Content>
+                                <Link href={`/classes/${zipped[1].id}`}>
+                                  <Button
+                                    style={{
+                                      fontSize: '1vw',
+                                      width: '100%',
+                                      minWidth: '41px',
+                                    }}
+                                    color={zipped[0]}
+                                    content={zipped[1].name}
+                                  />
+                                </Link>
+                              </List.Content>
+                            </List.Item>
+                          ))}
+                      </List>
+                    </div>
+                  ),
+                },
+              },
+            ]}
+          />
+        </div>
+        <div
+          style={{
+            position: 'absolute',
+            width: 'calc(100% - 68px)',
+            bottom: 14,
+            fontSize: '1vw',
+            minWidth: '41px',
+          }}
+        >
+          <ModifyClassesModal onSelect={this.modifyClasses} />
+        </div>
+      </>
+    )
+  }
 }
 
 export default DropDown
---
+++ components/modifyClassesModal.js
@@ -0,0 +1,179 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import {
+  Modal,
+  Button,
+  Checkbox,
+  Table,
+  Header,
+  Input,
+} from 'semantic-ui-react'
+import _ from 'lodash'
+import * as api from '../util/mercuryService'
+
+class ModifyClassesModal extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      modalState: false,
+      classRoles: [],
+    }
+  }
+  componentDidMount() {
+    const classRoles = []
+    api
+      .getMe()
+      .then((meData) => {
+        this.user = meData
+      })
+      .then(() => api.getClasses())
+      .then((classes) => {
+        classes.map((c) => {
+          api.getClass(c.id).then((c) => {
+            const userRole = c.users.find((u) => u.id === this.user.id)
+            if (!userRole) {
+              classRoles.push({
+                classId: c.id,
+                className: c.name,
+                classRole: '',
+              })
+              console.log(classRoles)
+            } else {
+              const { role } = userRole
+              classRoles.push({
+                classId: c.id,
+                className: c.name,
+                classRole: role,
+              })
+              console.log(classRoles)
+            }
+          })
+        })
+        console.log(classRoles)
+        this.setState({ classRoles })
+      })
+
+      .catch(console.error)
+  }
+
+  handleSubmit = () => {
+    this.setState({ modalState: false })
+    // this.props.onSubmit(this.state.classRoles)
+  }
+
+  getTACell(classRole) {
+    return classRole === 'Student' ? (
+      <Input />
+    ) : classRole === '' ? (
+      <></>
+    ) : (
+      <Header as="h4">
+        <Header.Content>{'verified ' + classRole}</Header.Content>
+      </Header>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <Modal
+          style={{
+            borderless: 'true',
+            width: '40%',
+            height: '40%',
+          }}
+          trigger={
+            <Button
+              color="teal"
+              content="Modify Class"
+              fluid
+              style={{ fontSize: '1vw' }}
+              onClick={() =>
+                this.setState({ groupName: '', value: '', modalState: true })
+              }
+            />
+          }
+          open={this.state.modalState}
+          onClose={() => this.setState({ modalState: false })}
+          closeOnDimmerClick={false}
+          closeOnEscape={false}
+          closeIcon
+        >
+          <Modal.Content style={{ borderless: 'true' }}>
+            <div
+              style={{
+                textAlign: 'center',
+                padding: '5%',
+                height: '50%',
+                overflowY: 'auto',
+              }}
+            >
+              <Table basic="very" celled>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell style={{ textAlign: 'center' }}>
+                      Classes
+                    </Table.HeaderCell>
+                    <Table.HeaderCell style={{ textAlign: 'center' }}>
+                      Student
+                    </Table.HeaderCell>
+                    <Table.HeaderCell style={{ textAlign: 'center' }}>
+                      TA
+                    </Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                  {this.state.classRoles.map((c) => (
+                    <Table.Row key={c.classId}>
+                      <Table.Cell style={{ textAlign: 'center' }}>
+                        <Header as="h4">
+                          <Header.Content>{c.className}</Header.Content>
+                        </Header>
+                      </Table.Cell>
+                      <Table.Cell style={{ textAlign: 'center' }}>
+                        <Checkbox
+                          disabled={
+                            c.classRole === 'Professor' || c.classRole === 'TA'
+                          }
+                          checked={c.classRole === 'Student'}
+                        />
+                      </Table.Cell>
+                      <Table.Cell style={{ textAlign: 'center' }}>
+                        {this.getTACell(c.classRole)}
+                      </Table.Cell>
+                    </Table.Row>
+                  ))}
+                </Table.Body>
+              </Table>
+            </div>
+
+            <div
+              style={{
+                textAlign: 'center',
+                width: '70%',
+                height: '70%',
+                margin: 'auto',
+                padding: '5%',
+              }}
+            >
+              <Button
+                color="teal"
+                style={{ width: '50%', fontSize: '1vw' }}
+                onClick={this.handleSubmit}
+                content={'Done'}
+              />
+            </div>
+          </Modal.Content>
+        </Modal>
+      </div>
+    )
+  }
+}
+
+ModifyClassesModal.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+}
+
+export default ModifyClassesModal
