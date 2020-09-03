f0cadab7c918f45a18ec812faf3c89e1ef456fb3
2xOu7 <jonathanou1998@gmail.com>
2020-09-03 15:11:46 -0400

    refactor

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,6 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
-
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet()
